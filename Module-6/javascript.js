9d7493f2a55b1446c641278cb68c661b5cfa4d1c
JosMuse <no-reply>
2020-09-14 14:41:04 -0600

    All Prework files

---
+++ Module-6/javascript.js
@@ -0,0 +1,29 @@
+document.getElementById("button1").addEventListener("click" , function(){
+    document.getElementById("box").style.height = "250px";
+});
+document.getElementById("button2").addEventListener("click" , function(){
+    document.getElementById("box").style.backgroundColor = "blue";
+});
+document.getElementById("button3").addEventListener("click" , function(){
+    document.getElementById("box").style.opacity = ".5";
+});
+document.getElementById("button5").addEventListener("click" , function(){
+    document.getElementById("box").style.boxShadow = "10px 20px 30px black";
+});
+
+
+
+//<--resets-->
+
+document.getElementById("button4").addEventListener("click" , function(){
+    document.getElementById("box").style.backgroundColor = "orange";
+});
+document.getElementById("button4").addEventListener("click" , function(){
+    document.getElementById("box").style.height = "150px";
+});
+ document.getElementById("button4").addEventListener("click" , function(){
+    document.getElementById("box").style.opacity = "1";
+});
+document.getElementById("button4").addEventListener("click" , function(){
+    document.getElementById("box").style.boxShadow = "0px 0px 0px black";
+});
